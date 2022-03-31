ecdc25d60e05e862b83e19aed93489df290b93a4
sbwcwso <lijunjie199502@gmail.com>
2022-04-01 01:13:55 +0800

    finish lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,18 @@ public class LeapYear {
         }
     }
 
+    /** Check if the given year is a leap year.
+     *  @param  year to be analyzed
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
