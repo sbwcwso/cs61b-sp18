268d3461ed296798a54b3f546768d45851c9ff52
sbwcwso <lijunjie199502@gmail.com>
2024-01-10 22:20:28 +0800

    finish lab2

--- lab2/DebugPractice/DebugExercise3.java
+++ lab2/DebugPractice/DebugExercise3.java
@@ -2,7 +2,7 @@
  * Created by jug on 1/22/18.
  */
 public class DebugExercise3 {
-    public static int countTurnips(In in) {
+    public static long countTurnips(In in) {
         int totalTurnips = 0;
         while (!in.isEmpty()) {
             String vendor = in.readString();
@@ -10,8 +10,7 @@ public class DebugExercise3 {
             double cost = in.readDouble();
             int numAvailable = in.readInt();
             if (foodType.equals("turnip")) {
-                int newTotal = totalTurnips + numAvailable;
-                totalTurnips = newTotal;
+                totalTurnips = totalTurnips + numAvailable;
             }
             in.readLine();
         }
--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -5,7 +5,7 @@ import java.util.Formatter;
  * with a large number of additional methods.
  *
  * @author P. N. Hilfinger, with some modifications by Josh Hug and melaniecebula
- *         [Do not modify this file.]
+ * [Do not modify this file.]
  */
 public class IntList {
     /**
@@ -29,7 +29,7 @@ public class IntList {
      * A List with null rest, and first = 0.
      */
     public IntList() {
-    /* NOTE: public IntList () { }  would also work. */
+        /* NOTE: public IntList () { }  would also work. */
         this(0, null);
     }
 
@@ -81,8 +81,15 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A == null) return B;
+
+        if (A.rest == null) {
+            A.rest = B;
+        } else {
+            A.rest = dcatenate(A.rest, B);
+        }
+
+        return A;
     }
 
     /**
@@ -90,23 +97,17 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A == null) {
+            if (B == null) {
+                return null;
+            } else {
+                return new IntList(B.first, catenate(null, B.rest));
+            }
+        }
+
+        return new IntList(A.first, catenate(A.rest, B));
+
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     /**
